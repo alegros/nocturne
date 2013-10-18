762e5275d5dd56bcd868d13bd83f2a4901bf63cf
Alexandre Le Gros <alexandre.lgr@gmail.com>
2013-10-18 11:24:27 +0200

    Moved the javascript files in static/ and added a change log html page

---
+++ static/functions.js
@@ -0,0 +1,25 @@
+function demon()
+{
+    var selected_demon = $("#demon_list option:selected").val();
+    var response = $.post("/demonList", {demon:selected_demon}, function(){
+        $("#container").html(response.responseText);
+    });
+}
+
+function fusion()
+{
+    var selected_d1 = $("#d1 option:selected").val();
+    var selected_d2 = $("#d2 option:selected").val();
+    var response = $.post("/fusion", {d1:selected_d1, d2:selected_d2}, function(){
+        $("#container").html(response.responseText);
+    });
+}
+
+function reverse() 
+{
+    var child = $("#child option:selected").val();
+    var parent1 = $("#parent1 option:selected").val();
+    var response = $.post("/reverseFusion", {child:child, parent1:parent1}, function(){
+        $("#container").html(response.responseText);
+    });
+}
